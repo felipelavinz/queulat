ec106a883a6117df3efd0d941bc1f49b270980ae
Felipe Lavín Z <felipe@bloom-ux.com>
2018-03-09 11:53:57 -0300

    removed unused code from input text; added input email

---
+++ src/forms/elements/class-input-email.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Queulat\Forms\Element;
+
+class Input_Email extends Input {
+	protected static $type = 'email';
+}
--- src/forms/elements/class-input-text.php
+++ src/forms/elements/class-input-text.php
@@ -4,10 +4,4 @@ namespace Queulat\Forms\Element;
 
 class Input_Text extends Input {
 	protected static $type = 'text';
-	public static function __callStatic( $method, $arguments ) {
-		var_dump( func_get_args() );
-		exit;
-		$obj = new static();
-		return $obj->$method( ...$arguments );
-	}
 }

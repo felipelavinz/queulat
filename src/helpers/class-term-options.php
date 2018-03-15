45777fdb406ec7ad3d8b58ff5a3e7255ad0f1dc9
Felipe Lavín Z <felipe@bloom-ux.com>
2018-03-15 13:15:12 -0300

    added a new helper to build option arrays from a get_terms call

---
+++ src/helpers/class-term-options.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Queulat\Helpers;
+
+class Term_Options extends \ArrayIterator {
+	private $terms = [];
+	public function __construct( array $args, array $flags = [] ) {
+		$args = wp_parse_args( $args, [
+			'orderby' => 'name',
+			'order'   => 'ASC'
+		]);
+		$this->terms = get_terms( $args );
+		if ( isset( $flags['show_option_none'] ) && $flags['show_option_none'] ) {
+			array_unshift( $this->terms, (object)[
+				'name'    => _x('(None)', 'null term option', 'queulat'),
+				'term_id' => 0
+			]);
+		}
+		parent::__construct( $this->terms );
+	}
+	public function current() {
+		return parent::current()->name;
+	}
+	public function key() {
+		$key = parent::key();
+		return $this->terms[ $key ]->term_id;
+	}
+}

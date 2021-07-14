b2fc40a7bc12882a38efb61824cb8fd3c48cd2c4
Felipe Lavín Z <felipe@bloom-ux.com>
2021-07-14 11:18:54 -0400

    add custom argument to select2 controls to keep selected items order

--- src/forms/elements/js/element-ui-select2.js
+++ src/forms/elements/js/element-ui-select2.js
@@ -3,8 +3,22 @@
 		function(){
 			$( '.select-2' ).each(
 				function(){
-					var args = $.extend( true, { }, $( this ).data( 'select2-args' ) );
+					/**
+					 * By default, select2 reorders the selected options.
+					 * keepSelectionsOrder:true makes that each new selection is appended
+					 */
+					var args = $.extend( true, {
+						keepSelectionsOrder: true
+					}, $( this ).data( 'select2-args' ) );
 					$( this ).select2( args );
+					if ( args.keepSelectionsOrder ) {
+						$( this ).on( 'select2:select', function( e ){
+							var id = e.params.data.id;
+							var option = $(e.target).children('[value="'+ id +'"]');
+							option.detach();
+							$( e.target ).append( option ).change();
+						} );
+					}
 				}
 			);
 		}
